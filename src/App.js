65764f6ce8143d040533727111df17cbe287f791
Mubina <mubina.pathan.mmp@gmail.com>
2026-02-20 00:29:46 +0530

    Initial commit - Hello Noviqo project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>Hello Noviqo</h1>
+      <p>My DevOps Deployment Project</p>
+    </div>
+  );
+}
+
+export default App;
